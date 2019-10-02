42b8aae4361a38bed48e17fc2cd8c01dadbabea1
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-10-02 09:55:53 +0530

    created the  class add method

--- src/main/java/com/main/sqlcrud/controller/RestApis.java
+++ src/main/java/com/main/sqlcrud/controller/RestApis.java
@@ -10,13 +10,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.validation.Valid;
 
+import com.main.sqlcrud.message.request.NewClassForm;
 import com.main.sqlcrud.message.request.StudentForm;
 import com.main.sqlcrud.message.response.ResponseMessage;
 import com.main.sqlcrud.model.Student;
+import com.main.sqlcrud.model.SingleClass;
+import com.main.sqlcrud.repository.ClassRepository;
 import com.main.sqlcrud.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +36,9 @@ public class RestApis {
 
     @Autowired
     StudentRepository studentRepository;
+
+    @Autowired
+    ClassRepository classRepository;
 
     @PostMapping("/add")
     public ResponseEntity<?> addingUser(@Valid @RequestBody StudentForm studentRequest) {
@@ -46,8 +55,8 @@ public class RestApis {
         // "+studentRequest.getEnrolledDate());
 
         // Creating user's account
-        Student student = new Student(studentRequest.getFirstName(), studentRequest.getLastName(),
-                studentRequest.getBday(), studentRequest.getAddress(), studentRequest.getAdmissionNumber(),
+        Student student = new Student(studentRequest.getAdmissionNumber(),studentRequest.getFirstName(), studentRequest.getLastName(),
+                studentRequest.getBday(), studentRequest.getAddress(), 
                 studentRequest.getEnrolledDate());
         studentRepository.save(student);
         // User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
@@ -98,6 +107,41 @@ public class RestApis {
         }
 
     }
+
+
+    @PostMapping("/addClass")
+    public ResponseEntity<?> addingClass(@Valid @RequestBody NewClassForm newClassRequest) {
+
+
+        List<Long> strStudents = newClassRequest.getStudents();
+
+        System.out.println("received students : "+strStudents+"  "+"class name: "
+                        +newClassRequest.getClassName()+" teacher : "+newClassRequest.getTeacherId());
+
+        Set<Student> students = new HashSet<>();
+
+        strStudents.forEach(admissionNum -> {
+
+                Student tempStudent = studentRepository.findByAdmissionNumber(admissionNum);
+
+                if(tempStudent == null){
+                    System.out.println("No students found.");
+                }else{
+                    System.out.println(" found student : "+tempStudent.getAdmissionNumber()+"  "+tempStudent.getFirstName()+" "+tempStudent.getAdmissionNumber());
+                }
+
+                students.add(tempStudent);
+
+		});
+
+        SingleClass tempClass = new SingleClass(newClassRequest.getClassName(),newClassRequest.getTeacherId());
+        tempClass.setStudents(students);
+
+        classRepository.save(tempClass);
+
+        return new ResponseEntity<>(new ResponseMessage("api worked!"), HttpStatus.CREATED);
+    }
+
 
     
 }
